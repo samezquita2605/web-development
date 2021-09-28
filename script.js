01eb8d7e040815e9b7a80e99f092d00c73d4db65
Sebastian Amezquita <samezquita2605@gmail.com>
2021-09-28 11:29:59 -0500

    initial commit

--- script.js
+++ script.js
@@ -41,10 +41,8 @@ toggleSwitch.addEventListener('change', switchTheme);
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-
     if (currentTheme === 'dark ') {
         toggleSwitch.checked = true;
         toggleDarkLightMode(true);
     }
-
 }
